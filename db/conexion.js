bed247a3e1c718a1861a79dcbb00a1888f904a28
nancycrojas <nanclarisaok@gmail.com>
2026-05-05 19:21:19 -0300

    add dependencies and specialties bread

---
+++ db/conexion.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2/promise";
+
+process.loadEnvFile();
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  connectionLimit: 10,
+});
---
+++ db/test-conexion.js
@@ -0,0 +1,23 @@
+import { pool } from "./conexion.js";
+
+export async function testConexion() {
+  try {
+    const con = await pool.getConnection();
+    console.log("Conexión con base de datos OK");
+
+    const [result] = await con.query(
+      "SELECT NOW() AS hora_servidor, DATABASE() AS base_datos",
+    );
+    console.log("Datos de prueba");
+    console.table(result);
+
+    con.release();
+  } catch (error) {
+    console.log("Error al conectarse a la base de datos", error);
+    console.error({
+      codigo: error.code,
+      msg: error.message,
+    });
+    process.exit(1);
+  }
+}
